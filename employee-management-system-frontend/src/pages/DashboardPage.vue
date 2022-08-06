9ebfffae7abc813a38a50d53271000ff81b0c089
Jakub Cendrowski <66736611+Quuba@users.noreply.github.com>
2022-08-06 23:28:42 +0200

    improved dashboard page

--- employee-management-system-frontend/src/pages/DashboardPage.vue
+++ employee-management-system-frontend/src/pages/DashboardPage.vue
@@ -4,31 +4,30 @@
     <q-header bordered class="bg-primary text-white" height-hint="98">
       <q-toolbar>
         <q-toolbar-title>
-          <q-avatar>
-            <img src="https://cdn.quasar.dev/logo-v2/svg/logo-mono-white.svg">
-          </q-avatar>
-          Title
+          "Department" Department
         </q-toolbar-title>
+        <q-btn align="right" color="grey-1" text-color="primary" @click="logout">
+          Logout
+        </q-btn>
       </q-toolbar>
 
-      <q-tabs align="left">
-        <q-route-tab to="/page1" label="Page One" />
-        <q-route-tab to="/page2" label="Page Two" />
-        <q-route-tab to="/page3" label="Page Three" />
+      <q-tabs align="left" v-model="tab" inline-label>
+        <q-tab name="employees" label="Employees"/>
       </q-tabs>
     </q-header>
 
     <q-page-container>
-      <router-view />
+      <q-tab-panels v-model="tab">
+        <q-tab-panel name="employees">
+          Hello, this is employees tab
+        </q-tab-panel>
+      </q-tab-panels>
     </q-page-container>
 
     <q-footer elevated class="bg-grey-8 text-white">
       <q-toolbar>
         <q-toolbar-title>
-          <q-avatar>
-            <img src="https://cdn.quasar.dev/logo-v2/svg/logo-mono-white.svg">
-          </q-avatar>
-          <div>Title</div>
+          <div></div>
         </q-toolbar-title>
       </q-toolbar>
     </q-footer>
@@ -38,9 +37,11 @@
 
 <script setup lang="ts">
 import UserAccountService from "@/services/UserAccountService";
-import { onMounted } from "vue";
+import { onMounted, ref } from "vue";
 import { useRouter } from "vue-router";
 const router = useRouter();
+
+const tab = ref('employees')
 
 onMounted(() => {
   if (!UserAccountService.IsLogged()) {
