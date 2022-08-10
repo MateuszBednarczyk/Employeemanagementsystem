27696b5b973aec2362b318a1b4a503053fdb69c5
Mateusz Bednarczyk <m.bednarczyk@zsek.olsztyn.pl>
2022-08-10 20:43:37 +0200

    cors disabled

--- EmployeeManagementSystem/src/main/java/com/matthew/employeemanagementsystem/api/UserController.java
+++ EmployeeManagementSystem/src/main/java/com/matthew/employeemanagementsystem/api/UserController.java
@@ -8,12 +8,14 @@ import com.matthew.employeemanagementsystem.service.user.UserManagementService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
+@CrossOrigin(origins = "*")
 class UserController {
     private final UserManagementService userManagementService;
 
--- EmployeeManagementSystem/src/main/java/com/matthew/employeemanagementsystem/configuration/SecurityConfiguration.java
+++ EmployeeManagementSystem/src/main/java/com/matthew/employeemanagementsystem/configuration/SecurityConfiguration.java
@@ -15,6 +15,9 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+
+import java.util.List;
 
 @Configuration
 @EnableWebSecurity
@@ -63,5 +66,11 @@ class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .disable()
                 .csrf()
                 .disable();
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.setAllowedHeaders(List.of("Authorization", "Cache-Control", "Content-Type"));
+        corsConfiguration.setAllowedOrigins(List.of("*"));
+        corsConfiguration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PUT", "OPTIONS", "PATCH", "DELETE"));
+        corsConfiguration.setAllowCredentials(true);
+        corsConfiguration.setExposedHeaders(List.of("Authorization"));
     }
 }
