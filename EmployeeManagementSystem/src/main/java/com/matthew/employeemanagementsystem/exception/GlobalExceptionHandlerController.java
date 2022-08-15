cc1c1d34a294cba9c55506aace87d5320a3398b4
Mateusz Bednarczyk <m.bednarczyk@zsek.olsztyn.pl>
2022-08-15 23:10:10 +0200

    exception handling reworked

--- EmployeeManagementSystem/src/main/java/com/matthew/employeemanagementsystem/exception/GlobalExceptionHandlerController.java
+++ EmployeeManagementSystem/src/main/java/com/matthew/employeemanagementsystem/exception/GlobalExceptionHandlerController.java
@@ -1,5 +1,16 @@
 package com.matthew.employeemanagementsystem.exception;
 
+import com.matthew.employeemanagementsystem.exception.department.DepartmentAlreadyExistsException;
+import com.matthew.employeemanagementsystem.exception.department.DepartmentNoPermissionException;
+import com.matthew.employeemanagementsystem.exception.department.DepartmentNotFoundException;
+import com.matthew.employeemanagementsystem.exception.employee.EmployeeAlreadyExistsException;
+import com.matthew.employeemanagementsystem.exception.employee.EmployeeNotFoundException;
+import com.matthew.employeemanagementsystem.exception.role.RoleDoesntHavePermissionToThisFeatureException;
+import com.matthew.employeemanagementsystem.exception.role.RoleNotFoundException;
+import com.matthew.employeemanagementsystem.exception.role.UnexpectedRoleValue;
+import com.matthew.employeemanagementsystem.exception.user.EmptyPasswordException;
+import com.matthew.employeemanagementsystem.exception.user.UserDoesNotHavePermissionException;
+import com.matthew.employeemanagementsystem.exception.user.UsernameTakenException;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -8,9 +19,75 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestControllerAdvice
 public class GlobalExceptionHandlerController {
 
-    @ExceptionHandler(Throwable.class)
+    @ExceptionHandler(GeneralException.class)
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-    public String catchAndThrowExceptionMessage(Exception e) {
+    public String generalException(GeneralException e) {
+        return e.getMessage();
+    }
+
+    @ExceptionHandler(DepartmentAlreadyExistsException.class)
+    @ResponseStatus(HttpStatus.CONFLICT)
+    public String departmentAlreadyExistsException(DepartmentAlreadyExistsException e) {
+        return e.getMessage();
+    }
+
+    @ExceptionHandler(DepartmentNoPermissionException.class)
+    @ResponseStatus(HttpStatus.FORBIDDEN)
+    public String departmentNoPermissionException(DepartmentNoPermissionException e) {
+        return e.getMessage();
+    }
+
+    @ExceptionHandler(DepartmentNotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public String departmentNotFoundException(DepartmentNotFoundException e) {
+        return e.getMessage();
+    }
+
+    @ExceptionHandler(EmployeeAlreadyExistsException.class)
+    @ResponseStatus(HttpStatus.CONFLICT)
+    public String employeeAlreadyExistsException(EmployeeAlreadyExistsException e){
+        return e.getMessage();
+    }
+
+    @ExceptionHandler(EmployeeNotFoundException.class)
+    @ResponseStatus(HttpStatus.CONFLICT)
+    public String employeeNotFoundException(EmployeeNotFoundException e){
+        return e.getMessage();
+    }
+
+    @ExceptionHandler(RoleDoesntHavePermissionToThisFeatureException.class)
+    @ResponseStatus(HttpStatus.FORBIDDEN)
+    public String roleDoesntHavePermissionToThisFeatureException(RoleDoesntHavePermissionToThisFeatureException e){
+        return e.getMessage();
+    }
+
+    @ExceptionHandler(RoleNotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public String roleNotFoundException(RoleNotFoundException e){
+        return e.getMessage();
+    }
+
+    @ExceptionHandler(UnexpectedRoleValue.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public String unexpectedRoleValue(UnexpectedRoleValue e){
+        return e.getMessage();
+    }
+
+    @ExceptionHandler(EmptyPasswordException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public String emptyPasswordException(EmptyPasswordException e){
+        return e.getMessage();
+    }
+
+    @ExceptionHandler(UserDoesNotHavePermissionException.class)
+    @ResponseStatus(HttpStatus.FORBIDDEN)
+    public String userDoesNotHavePermissionException(UserDoesNotHavePermissionException e){
+        return e.getMessage();
+    }
+
+    @ExceptionHandler(UsernameTakenException.class)
+    @ResponseStatus(HttpStatus.CONFLICT)
+    public String usernameTakenException(UsernameTakenException e){
         return e.getMessage();
     }
 
