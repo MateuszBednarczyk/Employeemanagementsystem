b75552a56d07c4917fa15110fb0509eba83ad265
Mateusz Bednarczyk <m.bednarczyk@zsek.olsztyn.pl>
2022-08-17 15:40:41 +0200

    add controller, which redirect to frontend, when user is trying to reach api uncorrectly

--- EmployeeManagementSystem/src/main/java/com/matthew/employeemanagementsystem/api/LoginController.java
+++ EmployeeManagementSystem/src/main/java/com/matthew/employeemanagementsystem/api/LoginController.java
@@ -1,24 +1,22 @@
 package com.matthew.employeemanagementsystem.api;
 
-import org.springframework.stereotype.Controller;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
-class LoginController {
+import javax.servlet.http.HttpServletRequest;
+import java.net.URI;
 
+@RestController
+@Slf4j
+public class LoginController {
     @GetMapping("")
-    public String redirectToLoginWhenUserDidntPassEndpoint() {
-        return "login.html";
+    ResponseEntity<Void> redirect(HttpServletRequest request) {
+        return ResponseEntity
+                .status(HttpStatus.FOUND)
+                .location(URI.create("http://" + request.getServerName() + ":8081/"))
+                .build();
     }
-
-    @GetMapping("/login")
-    public String showLoginForm() {
-        return "login.html";
-    }
-
-    @GetMapping("/home")
-    public String successLogin() {
-        return "home.html";
-    }
-
 }
---
+++ EmployeeManagementSystem/src/main/java/com/matthew/employeemanagementsystem/domain/entities/ProjectEntity.java
@@ -0,0 +1,5 @@
+package com.matthew.employeemanagementsystem.domain.entities;
+
+public class ProjectEntity {
+    //created it here to leak some new features :D :P
+}
