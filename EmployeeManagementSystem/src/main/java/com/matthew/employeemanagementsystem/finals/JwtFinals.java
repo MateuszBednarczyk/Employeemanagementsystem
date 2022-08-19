8626634ab8d8eeb878cefe774186e583c7b7bf49
Mateusz Bednarczyk <m.bednarczyk@zsek.olsztyn.pl>
2022-08-20 00:21:17 +0200

    refresh token expiring date update

--- EmployeeManagementSystem/src/main/java/com/matthew/employeemanagementsystem/finals/JwtFinals.java
+++ EmployeeManagementSystem/src/main/java/com/matthew/employeemanagementsystem/finals/JwtFinals.java
@@ -3,7 +3,7 @@ package com.matthew.employeemanagementsystem.finals;
 public class JwtFinals {
     public static final String SECRET = "secret";
     public static final String TOKEN_PREFIX = "Bearer ";
-    public static final Integer TOKEN_LIFETIME = 3600;
+    public static final Integer TOKEN_LIFETIME = 216060;
     public static final Integer REFRESH_TOKEN_LIFETIME = 7200;
     public static final String CLAIMS = "roles";
 }
